41b590b3eba91e5f329c1031909e4dbc032a1a39
georgeosutton <46813782+georgeosutton@users.noreply.github.com>
2021-01-01 18:54:19 +0000

    Update login.js

--- JS/login.js
+++ JS/login.js
@@ -1,6 +1,6 @@
 const loginURL =
   "https://login.eveonline.com/v2/oauth/authorize/?response_type=code";
-const redirectURI = "redirect_uri=http://127.0.0.1:5500/EVE-App/";
+const redirectURI = "redirect_uri=https://eve-api.netlify.app/";
 const cidString = "client_id=7b8292b4cb3e4bd7bc350fb4d1aeda24";
 const scope =
   "scope=esi-industry.read_character_jobs.v1 esi-industry.read_corporation_jobs.v1 esi-corporations.read_corporation_membership.v1";
